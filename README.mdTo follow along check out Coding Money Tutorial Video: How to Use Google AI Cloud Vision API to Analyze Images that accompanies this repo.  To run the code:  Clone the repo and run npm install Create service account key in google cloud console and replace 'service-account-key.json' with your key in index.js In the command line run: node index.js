b96293a13acc2621e40dba3d8a502e1219fde1e7
abdul babar <145856372+babar-sheikh@users.noreply.github.com>
2025-08-10 19:29:06 +0500

    Create README.mdTo follow along check out Coding Money Tutorial Video: How to Use Google AI Cloud Vision API to Analyze Images that accompanies this repo.  To run the code:  Clone the repo and run npm install Create service account key in google cloud console and replace 'service-account-key.json' with your key in index.js In the command line run: node index.js

---
+++ README.mdTo follow along check out Coding Money Tutorial Video: How to Use Google AI Cloud Vision API to Analyze Images that accompanies this repo.  To run the code:  Clone the repo and run npm install Create service account key in google cloud console and replace 'service-account-key.json' with your key in index.js In the command line run: node index.js
@@ -0,0 +1,7 @@
+To follow along check out Coding Money Tutorial Video: How to Use Google AI Cloud Vision API to Analyze Images that accompanies this repo.
+
+To run the code:
+
+Clone the repo and run npm install
+Create service account key in google cloud console and replace 'service-account-key.json' with your key in index.js
+In the command line run: node index.js
